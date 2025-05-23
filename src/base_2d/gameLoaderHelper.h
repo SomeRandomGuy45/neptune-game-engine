1f19f3e1c9ee7493ec5432d60d3e423c38db4121
SomeRandomGuy45 <dragonlionmusic@gmail.com>
2025-05-22 19:04:34 -0500

    more and more

---
+++ src/base_2d/gameLoaderHelper.h
@@ -0,0 +1,16 @@
+#ifndef gameLoaderHelper
+#define gameLoaderHelper
+
+#include <string>
+
+namespace neptune {
+
+bool isVaildGame(const std::string& gameFolderPath);
+
+/*
+* Add more later maybe?
+*/
+
+};
+
+#endif
